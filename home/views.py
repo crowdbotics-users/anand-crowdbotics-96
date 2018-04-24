f46d00db10592da35d606d095b755933e4e46bc4
Saurav <sp.gharti@gmail.com>
2018-04-24 20:35:50 +0000

    add package django-allauth

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
 	{'name':'django-rest-framework', 'url': 'http://pypi.python.org/pypi/django-rest-framework/0.1.0'},
     ]
     context = {
