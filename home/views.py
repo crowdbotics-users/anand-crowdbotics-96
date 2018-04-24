c2041974613256fd78d47536e467e0cc3ee60084
Saurav <sp.gharti@gmail.com>
2018-04-24 20:35:41 +0000

    add package django-rest-framework

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-rest-framework', 'url': 'http://pypi.python.org/pypi/django-rest-framework/0.1.0'},
     ]
     context = {
         'title': 'anand-crowdbotics-96',
