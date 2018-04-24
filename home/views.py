fe44446483e0fbc4ffeee49e727dc6b23b2492a1
Saurav <sp.gharti@gmail.com>
2018-04-24 20:36:00 +0000

    add package askbot

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'askbot', 'url': 'http://pypi.python.org/pypi/askbot/0.10.2'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
 	{'name':'django-rest-framework', 'url': 'http://pypi.python.org/pypi/django-rest-framework/0.1.0'},
     ]
